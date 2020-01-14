b80e431b53a95375cd08f80fdef53e6be9a95a88
Rafael Lima <rafa_xlr@hotmail.com>
2020-01-14 16:51:04 -0300

    Estado e cidade

---
+++ src/main/java/com/cursospringboot/demo/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.cursospringboot.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cursospringboot.demo.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>  {
+
+}
