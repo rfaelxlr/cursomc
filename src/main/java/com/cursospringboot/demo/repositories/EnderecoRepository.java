072d16fc61f93efbe761a57994d61fde85a721ec
Rafael Lima <rafa_xlr@hotmail.com>
2020-01-15 10:35:06 -0300

    mapeamento cliente e endereco

---
+++ src/main/java/com/cursospringboot/demo/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.cursospringboot.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cursospringboot.demo.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>  {
+
+}
