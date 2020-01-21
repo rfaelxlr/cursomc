e204caac058ac4701dacb54b6ced58fdca00147d
Rafael Lima <rafa_xlr@hotmail.com>
2020-01-21 12:56:16 -0300

    correcoes update categoriaservice e construtor pagamento

--- src/main/java/com/cursospringboot/demo/services/CategoriaService.java
+++ src/main/java/com/cursospringboot/demo/services/CategoriaService.java
@@ -16,66 +16,64 @@ import com.cursospringboot.demo.repositories.CategoriaRepository;
 import com.cursospringboot.demo.services.exceptions.DataIntegrityException;
 import com.cursospringboot.demo.services.exceptions.ObjectNotFoundException;
 
-
-
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		
+
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-		
-		
-		}
-	
-	
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+
+	}
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
 			repo.deleteById(id);
-			
+
 		} catch (DataIntegrityViolationException e) {
-			
+
 			throw new DataIntegrityException("Não é possível deletar uma categoria que tem produtos");
 		}
-		
-	}
-	
-	public List<Categoria> findAll(){
-		return repo.findAll();
-		
-	}
-	
-	public Page<Categoria> findPage(Integer page, Integer linesPerPage,String orderBy,String direction){
-	
-		PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
-				orderBy);
-		
-		return repo.findAll(pageRequest);
-		
-	}
-		
-		
-	public Categoria fromDTO(CategoriaDTO objDTO) {
-		return new Categoria(objDTO.getId(),objDTO.getNome());
-	}
-	
-		
 
-	
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
+
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		return repo.findAll(pageRequest);
+
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
+	}
+
+	private void updateData(Categoria newObj, Categoria obj) {
+
+		newObj.setNome(obj.getNome());
+
+	}
+
 }
