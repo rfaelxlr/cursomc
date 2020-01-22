8d8e0601a09b263b3761f65a0693ac9b064a6fe3
Rafael Lima <rafa_xlr@hotmail.com>
2020-01-22 13:36:48 -0300

    validacao cpf ou cnpj

--- src/main/java/com/cursospringboot/demo/dto/ClienteNewDTO.java
+++ src/main/java/com/cursospringboot/demo/dto/ClienteNewDTO.java
@@ -2,6 +2,13 @@ package com.cursospringboot.demo.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.cursospringboot.demo.services.validation.ClienteInsert;
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 
 	/**
@@ -9,18 +16,35 @@ public class ClienteNewDTO implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min=5,max=120,message = "O tamanho deve ser entre 5 a 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Email(message="email inválido")
 	private String email;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String numero;
+	
 	private String complemento;
+	
 	private String bairro;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String cep;
 	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String telefone1;
+	
 	private String telefone2;
 	private String telefone3;
 
